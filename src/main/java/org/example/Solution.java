d527150aebdf5572d430717ca86ce6292976bf17
annt <annt@gft.com>
2022-12-12 13:02:13 +0000

    Done

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,61 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.LinkedList;
+
+public class Solution {
+    public Solution(){
+
+    }
+
+    public int longestValidParentheses(String s) {
+
+        int largest = 0;
+        int counter = 0;
+        int openBrackets = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+
+                openBrackets += 1;
+                counter += 1;
+            } else if (openBrackets != 0) {
+
+                openBrackets -= 1;
+                counter += 1;
+            } else{
+
+                counter = 0;
+            }
+
+            if (openBrackets == 0 && counter > largest){
+
+                largest = counter;
+            }
+        }
+
+        counter = 0;
+        openBrackets = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ')') {
+
+                openBrackets += 1;
+                counter += 1;
+            } else if (openBrackets != 0) {
+
+                openBrackets -= 1;
+                counter += 1;
+            } else{
+
+                counter = 0;
+            }
+
+            if (openBrackets == 0 && counter > largest) {
+
+                largest = counter;
+            }
+        }
+
+        return largest;
     }
 }
--- src/test/java/org/example/SolutionTest.java
+++ src/test/java/org/example/SolutionTest.java
@@ -1,2 +1,56 @@
-package org.example;public class SolutionTest {
+package org.example;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+
+    static Solution solution;
+
+    String s;
+    int result;
+
+    @BeforeAll
+    static void setup(){
+
+        solution = new Solution();
+    }
+
+    @Test
+    public void emptyString(){
+
+        result = 0;
+        s = "";
+
+        assertEquals(result, solution.longestValidParentheses(s));
+    }
+
+    @Test
+    public void oneValidParentheses(){
+
+        result = 2;
+        s = "()";
+
+        assertEquals(result, solution.longestValidParentheses(s));
+    }
+
+    @Test
+    public void oneValidParenthesesWithMoreThan2Parentheses(){
+
+        result = 2;
+        s = ")()))";
+
+        assertEquals(result, solution.longestValidParentheses(s));
+    }
+
+    @Test
+    public void multipleValidParentheses(){
+
+        result = 4;
+        s = ")()())";
+
+        assertEquals(result, solution.longestValidParentheses(s));
+    }
 }
